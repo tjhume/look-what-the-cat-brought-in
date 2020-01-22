a413772168dfb7db944eb5f6586ee5a1b00a6ce7
tjhume <tristanjhume@gmail.com>
2020-01-21 20:45:58 -0700

    Removed some redudant code/files

--- themes/look-cat-in/page-templates/shop-page.php
+++ themes/look-cat-in/page-templates/shop-page.php
@@ -5,20 +5,6 @@
 get_header(); ?>
 <section id="shop">
 
-<?php
-while ( have_posts() ) :
-	the_post();
-
-	get_template_part( 'template-parts/content', 'page' );
-
-	// If comments are open or we have at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) :
-		comments_template();
-	endif;
-
-endwhile; // End of the loop.
-?>
-
 </section>
 <?php
 get_footer();
