62c60b9ecc8968b1fe8a31ba82b1992b8c9166c0
tjhume <tristanjhume@gmail.com>
2020-02-06 22:16:49 -0700

    News page progress

---
+++ themes/look-cat-in/page-templates/news-page.php
@@ -0,0 +1,63 @@
+<?php
+/**
+* Template Name: News
+*/
+get_header(); 
+$options_id = get_theme_options_id(); ?>
+
+<section class="top" id="news">
+    <div class="news-wrapper">
+        <?php
+            $paged = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
+
+            $args = array(
+                'posts_per_page' => 9,
+                'post_type' => 'post',
+                'order' => "DESC",
+                'paged' => $paged
+            );
+
+            $news = new WP_Query($args);
+
+            if ($news -> have_posts()){ while($news -> have_posts()) : $news -> the_post();
+                //Post Content ?>
+
+            <div class="news-box">
+                <div class="news-content">
+                    <p><?php the_title(); ?></p>
+                </div>
+            </div>
+
+            <?php endwhile; ?>
+            
+            <div class="pagination">
+                <?php
+                    echo paginate_links( array(
+                        'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
+                        'total'        => $news->max_num_pages,
+                        'current'      => max( 1, get_query_var( 'paged' ) ),
+                        'format'       => '?paged=%#%',
+                        'show_all'     => false,
+                        'type'         => 'plain',
+                        'end_size'     => 2,
+                        'mid_size'     => 1,
+                        'prev_next'    => true,
+                        'prev_text'    => sprintf( '<i></i> %1$s', __( 'Newer Posts', 'look-cat-in' ) ),
+                        'next_text'    => sprintf( '%1$s <i></i>', __( 'Older Posts', 'look-cat-in' ) ),
+                        'add_args'     => false,
+                        'add_fragment' => '',
+                    ) );
+                ?>
+            </div>
+
+            <?php }else{
+                //If no posts are found ?>
+                <div id="latest-content">
+                <h2>No News Found</h2>
+                </div>
+            <?php } wp_reset_postdata();
+        ?>
+    </div>
+</section>
+
+<?php get_footer(); ?>
--- themes/look-cat-in/page-templates/shop-page.php
+++ themes/look-cat-in/page-templates/shop-page.php
@@ -3,8 +3,16 @@
 * Template Name: Shop
 */
 get_header(); ?>
-<section class="top" id="shop">
 
+<?php if(is_user_logged_in()){ ?>
+    <section class="top" id="shop">
+        <?php echo do_shortcode('[products limit="9" columns="3"]'); ?>
+    </section>
+<?php }else{ ?>
+<section class="top" id="shop">
+    <p class="soon">Coming Soon</p>
 </section>
+<?php } ?>
+
 <?php
 get_footer();
