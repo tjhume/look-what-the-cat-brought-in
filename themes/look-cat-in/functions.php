cc61d0455ab1eb7e34f6f254f8314556ec60c5d8
tjhume <tristanjhume@gmail.com>
2020-02-08 12:17:02 -0700

    News page styles

--- themes/look-cat-in/functions.php
+++ themes/look-cat-in/functions.php
@@ -132,6 +132,23 @@ function get_theme_options_id(){
 include_once('acf-repeater/acf-repeater.php');
 
 /**
+ * To get first image from a post
+ */
+function cat_get_first_image(){
+	global $post, $posts;
+	$first_img = '';
+	ob_start();
+	ob_end_clean();
+	$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
+	$first_img = $matches [1] [0];
+
+	if(empty($first_img)){ //Defines a default image
+		$first_img = "../wp-content/themes/look-cat-in/default.jpg";
+	}
+	return $first_img;
+}
+
+/**
  * Removes HTML Spacing above WP Admin Bar
  */
 function remove_admin_login_header(){
--- themes/look-cat-in/page-templates/news-page.php
+++ themes/look-cat-in/page-templates/news-page.php
@@ -6,6 +6,7 @@ get_header();
 $options_id = get_theme_options_id(); ?>
 
 <section class="top" id="news">
+    <h1>News</h1>
     <div class="news-wrapper">
         <?php
             $paged = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
@@ -21,10 +22,22 @@ $options_id = get_theme_options_id(); ?>
 
             if ($news -> have_posts()){ while($news -> have_posts()) : $news -> the_post();
                 //Post Content ?>
-
+            <?php
+                $excerpt = get_the_excerpt();
+                $excerpt = substr($excerpt, 0, strpos(wordwrap($excerpt, 100), "\n")) . '...';
+            ?>
             <div class="news-box">
                 <div class="news-content">
-                    <p><?php the_title(); ?></p>
+                    <div class="img-wrap" style="background-image: url('<?php echo cat_get_first_image(); ?>')"></div>
+                    <h2><?php the_title(); ?></h2>
+                    <p><?php echo $excerpt; ?></p>
+                    <time class="post-date">
+                        <?php if(get_field('use_custom_date')){
+                            the_field('date');
+                        }else{
+                            the_date();
+                        }?>
+                    </time>
                 </div>
             </div>
 
