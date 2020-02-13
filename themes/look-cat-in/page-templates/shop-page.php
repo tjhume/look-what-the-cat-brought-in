b4c6542d6b9ebd770902a61797b34e56be233eef
tjhume <tristanjhume@gmail.com>
2020-02-13 14:53:13 -0700

    Added shop page pagination

--- themes/look-cat-in/page-templates/shop-page.php
+++ themes/look-cat-in/page-templates/shop-page.php
@@ -7,7 +7,7 @@ get_header(); ?>
 <?php if(is_user_logged_in()){ ?>
     <section class="top" id="shop">
         <h1>Shop</h1>
-        <?php echo do_shortcode('[products limit="9" columns="3"]'); ?>
+        <?php echo do_shortcode('[products limit="9" columns="3" paginate="true"]'); ?>
     </section>
 <?php }else{ ?>
 <section class="top" id="shop">
