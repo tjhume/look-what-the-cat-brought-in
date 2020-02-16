141e76b30a2e1c1f18be9e48685a83f82d188902
tjhume <tristanjhume@gmail.com>
2020-02-15 20:38:18 -0700

    Updated static site

--- themes/look-cat-in/page-templates/shop-page.php
+++ themes/look-cat-in/page-templates/shop-page.php
@@ -4,16 +4,16 @@
 */
 get_header(); ?>
 
-<?php if(is_user_logged_in()){ ?>
+<?php //if(is_user_logged_in()){ ?>
     <section class="top" id="shop">
         <h1>Shop</h1>
         <?php echo do_shortcode('[products limit="9" columns="3" paginate="true"]'); ?>
     </section>
-<?php }else{ ?>
-<section class="top" id="shop">
+<?php //}else{ ?>
+<!--<section class="top" id="shop">
     <p class="soon">Coming Soon</p>
-</section>
-<?php } ?>
+</section>-->
+<?php //} ?>
 
 <?php
 get_footer();
